9b916a84b6bf2d341d31a9ad7c113c038c47aede
Neel Ratan Guria <neelratan@live.com>
2018-07-31 00:42:05 +0530

    001 UI Done

---
+++ app/src/main/java/com/devteam000/mathsgym/WelcomeScreen.java
@@ -0,0 +1,25 @@
+package com.devteam000.mathsgym;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class WelcomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+    }
+
+    public void play(View view)
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+}
